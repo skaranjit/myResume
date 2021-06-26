0fc2bb4fbc4a360765b12779fe1d4abfdd71024d
skaranjit <karanjitsu@mnstate.edu>
2021-06-25 21:30:28 -0500

    asda

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -129,7 +129,7 @@ class Profile extends Component {
   }
   updateCanvas() {
     const canvas = this.refs.canvas;
-    const frameCount = 3690;
+    const frameCount = 208;
     var imageObj1 = new Image();
     imageObj1 = this.getImage(1);
     const ctx = canvas.getContext("2d");
@@ -150,7 +150,7 @@ class Profile extends Component {
 
   getAllImg = async () => {
     var x = 0;
-    while (x != 3690) {
+    while (x != 208) {
       x++;
       const imgObj = new Image();
 
